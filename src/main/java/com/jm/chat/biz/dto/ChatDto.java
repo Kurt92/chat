7e02436fd0e6811ba46a0822ea020d79f87a4c89
kurt92 <chcch1234@gmail.com>
2025-05-20 16:42:55 +0900

    feat : 날짜파싱 서버에서는 일관되게 넘겨주고, 프론트에서 처리하도록 함

--- src/main/java/com/jm/chat/biz/dto/ChatDto.java
+++ src/main/java/com/jm/chat/biz/dto/ChatDto.java
@@ -22,6 +22,16 @@ public class ChatDto {
             private String message;
         }
 
+        @Getter
+        @Setter
+        public static class ChatRoomCreate {
+            private Long userId;
+            private Long targetId;
+            private String roomNm;
+        }
+
+
+
     }
 
 
@@ -38,17 +48,6 @@ public class ChatDto {
             private LocalDateTime lastMessageTime;
             private Long unreadCount;
 
-            @JsonProperty("lastMessageTime")
-            public String getFormattedLastMessageTime() {
-                LocalDate today = LocalDate.now();
-
-                if (lastMessageTime == null)
-                    return null;
-                if (lastMessageTime.toLocalDate().isEqual(today))
-                    return lastMessageTime.format(DateTimeFormatter.ofPattern("HH:mm"));
-                else
-                    return lastMessageTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-            }
         }
 
 
@@ -61,25 +60,6 @@ public class ChatDto {
             private String message;
             private LocalDateTime createDt;
 
-            @JsonProperty("createDt")
-            public String getFormattedMessageTime() {
-                if (createDt == null)
-                    return null;
-
-                LocalDateTime now = LocalDateTime.now();
-                LocalDate today = now.toLocalDate();
-
-                if (createDt.toLocalDate().isEqual(today)) {
-                    // 오늘
-                    return createDt.format(DateTimeFormatter.ofPattern("HH:mm"));
-                } else if (createDt.getYear() == now.getYear()) {
-                    // 같은 해
-                    return createDt.format(DateTimeFormatter.ofPattern("MM-dd HH:mm"));
-                } else {
-                    // 다른 해
-                    return createDt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-                }
-            }
 
         }
     }
